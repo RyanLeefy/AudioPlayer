40fd1c1dd97976eb56ba4096b3b1148ba32a47ae
lipuyusx <lipuyusx@gomo.com>
2017-01-20 19:10:07 +0800

    主界面设计

--- app/src/main/java/com/example/administrator/audioplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/audioplayer/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.administrator.audioplayer.activity;
 
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.administrator.audioplayer.R;
+import com.example.administrator.audioplayer.widget.SplashScreen;
 
 
 /**
@@ -11,12 +15,43 @@ import com.example.administrator.audioplayer.R;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private Handler mHandler = new Handler();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final SplashScreen mSplashScreen = new SplashScreen(this);
+        mSplashScreen.show(R.drawable.art_login_bg, SplashScreen.FADE_OUT);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
 
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSplashScreen.dismiss();
+            }
+        }, 0);
+
+        setToolbar();
+
     }
+
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.actionbar_menu);
+
+        //ab.setDisplayHomeAsUpEnabled(true);
+        //ab.setHomeAsUpIndicator(R.drawable.actionbar_menu);
+
+
+    }
+
+
 }
